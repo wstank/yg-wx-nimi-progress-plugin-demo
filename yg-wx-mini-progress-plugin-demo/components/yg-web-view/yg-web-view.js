2fbb0f69b0ba8e28eb8a17ab5496e0fd2368b61f
wstank <285719629@qq.com>
2021-10-29 21:50:04 +0800

    Add files via upload

---
+++ yg-wx-mini-progress-plugin-demo/components/yg-web-view/yg-web-view.js
@@ -0,0 +1,25 @@
+//自定义控件
+Component({
+
+    properties: {
+        //加载地址
+        url: {
+            type: String
+        }
+    },
+
+    data: {
+
+    },
+
+    methods: {
+
+        /**
+         * 网页加载错误
+         * @param {Object} error 错误信息
+         */
+        loadError(error) {
+            getApp().globalData.ygPlugin.reportError('load-ad-page', 'load page[' + this.properties.url + '] error:' + error.detail.errMsg);
+        },
+    }
+})
---
+++ yg-wx-mini-progress-plugin-demo/pages/index/index.js
@@ -0,0 +1,63 @@
+Page({
+  data: {
+    //设置扩展数据
+    ygExtData: {
+      //小程序图标地址（网络地址）
+      appIcon: 'https://img0.baidu.com/it/u=157449498,3253226716&fm=26&fmt=auto',
+      // 小程序名称
+      appName: '测试小程序',
+      //banner广告自动刷新间隔时间（秒，不设置或小于等于0s表示不自动刷新，大于0s表示具体间隔时间，但最小刷新时间不低于30s）
+      bannerAdDuration: 0,
+      //视频广告主题样式：提供黑白两种主题样式，取值white, black
+      videoAdTheme: 'white'
+    },
+
+    //是否展示插屏广告
+    showInterstitialAd: false,
+
+    //是否展示激励视频广告
+    showRewardVideoAd: false
+  },
+  onLoad() {
+
+  },
+
+  /**
+   * 请求插屏广告
+   */
+  requestInterstitialAd() {
+    this.setData({
+      showInterstitialAd: true
+    });
+  },
+
+  /**
+   * 请求激励视频广告
+   */
+  requestRewardVideoAd() {
+    this.setData({
+      showRewardVideoAd: true
+    });
+  },
+
+  onAdLoad(event) {
+    console.log('------------------ on ad[' + event.detail.adId + '] load ---------------------');
+  },
+
+  onAdShow(event) {
+    console.log('------------------ on ad[' + event.detail.adId + '] show ---------------------');
+  },
+
+  onAdClick(event) {
+    console.log('------------------ on ad[' + event.detail.adId + '] click ---------------------');
+  },
+
+  onAdClose(event) {
+    console.log('------------------ on ad[' + event.detail.adId + '] close ---------------------' + JSON.stringify(event.detail));
+  },
+
+  onAdError(event) {
+    console.log('------------------ on ad[' + event.detail.adId + '] error --------------------->' + JSON.stringify(event.detail));
+  }
+
+})
